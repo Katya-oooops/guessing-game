4eb1896d66d5eed3cd41f6e8e43e7025ea665da6
Katya-oooops <courage.wisdom.force@gmail.com>
2019-10-12 15:48:04 +0300

    fix: first

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,19 +2,24 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.min=min;
+        this.max=max;
+        return this.max, this.min;
     }
 
     guess() {
-
+        this.try=Math.ceil((this.max+this.min)/2);
+        return this.try
     }
 
     lower() {
-
+        this.max=this.try;
+        return this.max;
     }
 
     greater() {
-
+        this.min=this.try;
+        return this.min;
     }
 }
 
